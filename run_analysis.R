951fe15be39eaf89c1a55093f7f451315a32352c
meet2subha <32111146+meet2subha@users.noreply.github.com>
2017-11-15 13:23:21 -0500

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,56 @@
+# File reading:
+x_train <- read.table("./Data/Course3/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./Data/Course3/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./Data/Course3/UCI HAR Dataset/train/subject_train.txt")
+
+# Reading testing tables:
+x_test <- read.table("./Data/Course3/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./Data/Course3/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./Data/Course3/UCI HAR Dataset/test/subject_test.txt")
+
+# Reading feature vector:
+features <- read.table('./Data/Course3/UCI HAR Dataset/features.txt')
+
+# Reading activity labels:
+activityLabels = read.table('./Data/Course3/UCI HAR Dataset/activity_labels.txt')
+
+#columns
+colnames(x_train) <- features[,2] 
+colnames(y_train) <-"activityId"
+colnames(subject_train) <- "subjectId"
+      
+colnames(x_test) <- features[,2] 
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
+      
+colnames(activityLabels) <- c('activityId','activityType')
+
+#Data merge
+mrg_train <- cbind(y_train, subject_train, x_train)
+mrg_test <- cbind(y_test, subject_test, x_test)
+setAllInOne <- rbind(mrg_train, mrg_test)
+
+#Reading column names
+colNames <- colnames(setAllInOne)
+
+#Vector
+mean_and_std <- (grepl("activityId" , colNames) | 
+                 grepl("subjectId" , colNames) | 
+                 grepl("mean.." , colNames) | 
+                 grepl("std.." , colNames) 
+                 )
+
+#Subset
+setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
+
+setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
+                              by='activityId',
+                              all.x=TRUE)
+
+#New Tidy data set
+secTidySet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
+secTidySet <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
+
+
+#Create new file
+write.table(secTidySet, "secTidySet.txt", row.name=FALSE)
